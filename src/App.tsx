b8cf1dc0619b8f09ebaf8664f990211f0e11985f
Oluwatobiloba Adebayo <hardebaryorone@gmail.com>
2023-09-16 19:03:23 +0100

    updated new stuff

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss'
+import { BrowserRouter } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Dashboard />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
